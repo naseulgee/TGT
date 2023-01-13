a99ae03971edc98ffa20876527a372135c605135
naseulgee <thwndglgkwk@naver.com>
2023-01-14 03:13:47 +0900

    23.01.14 나슬기 시설 등록 페이지 작성중

--- src/main/java/paw/togaether/place/controller/PlaceController.java
+++ src/main/java/paw/togaether/place/controller/PlaceController.java
@@ -3,6 +3,7 @@ package paw.togaether.place.controller;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import paw.togaether.common.domain.CommandMap;
@@ -16,25 +17,25 @@ public class PlaceController {
 	@RequestMapping("/list")
 	public void place_list(CommandMap commandMap) {}
 	
-	/* 23.01.13 나슬기: 시설 등록 폼 메소드 */
-	@RequestMapping("/writeForm")
-	public void place_regform(CommandMap commandMap) {}
-	/* 23.01.13 나슬기: 시설 등록 처리 메소드 */
-	@RequestMapping("/write")
-	public void place_reg(CommandMap commandMap) {}
-	
 	/* 23.01.13 나슬기: 시설 상세보기 메소드 */
 	@RequestMapping("/detail/{pl_idx}")
 	public void place_detail(@PathVariable("pl_idx") int cafe_idx, CommandMap commandMap) {}
 	
 	/* 23.01.13 나슬기: 시설 등록 폼 메소드 */
+	@RequestMapping("/writeForm")
+	public void place_regform(CommandMap commandMap) {}
+	/* 23.01.13 나슬기: 시설 등록 처리 메소드 */
+	@PostMapping("/write")
+	public void place_reg(CommandMap commandMap) {}
+	
+	/* 23.01.13 나슬기: 시설 수정 폼 메소드 */
 	@RequestMapping("/modifyForm")
 	public void place_modiform(CommandMap commandMap) {}
-	/* 23.01.13 나슬기: 시설 등록 처리 메소드 */
-	@RequestMapping("/modify")
+	/* 23.01.13 나슬기: 시설 수정 처리 메소드 */
+	@PostMapping("/modify")
 	public void place_modi(CommandMap commandMap) {}
 	
 	/* 23.01.13 나슬기: 시설 삭제 처리 메소드 */
-	@RequestMapping("/delete")
+	@PostMapping("/delete")
 	public void place_delete_req(CommandMap commandMap) {}
 }
---
+++ src/main/webapp/resources/js/place/form.js
@@ -0,0 +1,9 @@
+function search_addr(){
+	console.log(111);
+	//카카오 API를 통한 주소얻기
+	//주소를 input에 세팅
+}
+
+$(document).ready(function(){
+	
+});
