d2e0a18aa34390665de8008ecd57cffa4cabcd4f
naseulgee <thwndglgkwk@naver.com>
2023-01-17 21:38:47 +0900

    23.01.17 나슬기 시설, 카테고리, 메뉴 관련 DAO 및 Service 클래스 작성 완료

--- src/main/java/paw/togaether/place/controller/PlaceController.java
+++ src/main/java/paw/togaether/place/controller/PlaceController.java
@@ -13,29 +13,29 @@ import paw.togaether.common.domain.CommandMap;
 public class PlaceController {
 	Logger log = Logger.getLogger(this.getClass());
 	
-	/* 23.01.13 나슬기: 시설 리스트 메소드 */
+	/** 23.01.13 나슬기: 시설 리스트 메소드 */
 	@RequestMapping("/list")
 	public void place_list(CommandMap commandMap) {}
 	
-	/* 23.01.13 나슬기: 시설 상세보기 메소드 */
+	/** 23.01.13 나슬기: 시설 상세보기 메소드 */
 	@RequestMapping("/detail/{pl_idx}")
 	public void place_detail(@PathVariable("pl_idx") int cafe_idx, CommandMap commandMap) {}
 	
-	/* 23.01.13 나슬기: 시설 등록 폼 메소드 */
+	/** 23.01.13 나슬기: 시설 등록 폼 메소드 */
 	@RequestMapping("/writeForm")
 	public void place_regform(CommandMap commandMap) {}
-	/* 23.01.13 나슬기: 시설 등록 처리 메소드 */
+	/** 23.01.13 나슬기: 시설 등록 처리 메소드 */
 	@PostMapping("/write")
 	public void place_reg(CommandMap commandMap) {}
 	
-	/* 23.01.13 나슬기: 시설 수정 폼 메소드 */
+	/** 23.01.13 나슬기: 시설 수정 폼 메소드 */
 	@RequestMapping("/modifyForm")
 	public void place_modiform(CommandMap commandMap) {}
-	/* 23.01.13 나슬기: 시설 수정 처리 메소드 */
+	/** 23.01.13 나슬기: 시설 수정 처리 메소드 */
 	@PostMapping("/modify")
 	public void place_modi(CommandMap commandMap) {}
 	
-	/* 23.01.13 나슬기: 시설 삭제 처리 메소드 */
+	/** 23.01.13 나슬기: 시설 삭제 처리 메소드 */
 	@PostMapping("/delete")
 	public void place_delete_req(CommandMap commandMap) {}
 }
---
+++ src/main/java/paw/togaether/place/service/PlaceService.java
@@ -0,0 +1,37 @@
+package paw.togaether.place.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PlaceService {
+	//시설 리스트 & 페이징 & 검색
+	Map<String, Object>placeList(Map<String, Object> map) throws Exception;
+	//시설 상세
+	Map<String, Object>placeDetail(Map<String, Object> map) throws Exception;
+	//시설 글쓰기
+	int placeWrite(Map<String, Object> map) throws Exception;
+	//시설 수정
+	int placeModify(Map<String, Object> map) throws Exception;
+	//시설 삭제 요청
+	int placeDeleteReq(Map<String, Object> map) throws Exception;
+	//시설 삭제
+	int placeDelete(Map<String, Object> map) throws Exception;
+	
+	//시설 카테고리 리스트
+	List<Map<String, Object>>cateList(Map<String, Object> map) throws Exception;
+	//시설 카테고리 글쓰기
+	int cateWrite(Map<String, Object> map) throws Exception;
+	//시설 카테고리 수정
+	int cateModify(Map<String, Object> map) throws Exception;
+	//시설 카테고리 삭제
+	int cateDelete(Map<String, Object> map) throws Exception;
+	
+	//시설 메뉴 리스트
+	List<Map<String, Object>>menuList(Map<String, Object> map) throws Exception;
+	//시설 메뉴 글쓰기
+	int menuWrite(Map<String, Object> map) throws Exception;
+	//시설 메뉴 수정
+	int menuModify(Map<String, Object> map) throws Exception;
+	//시설 메뉴 삭제
+	int menuDelete(Map<String, Object> map) throws Exception;
+}
