afb1354e1ad384892b63769b6d207d5ac0c78793
ab9943 <ab9943@naver.com>
2023-01-18 19:26:11 +0900

    신현지 23.01.18 photo_SQL.xml이름변경

--- src/main/java/paw/togaether/review/service/ReviewServiceImpl.java
+++ src/main/java/paw/togaether/review/service/ReviewServiceImpl.java
@@ -1,12 +1,15 @@
 package paw.togaether.review.service;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import paw.togaether.common.util.FileUtils;
 import paw.togaether.review.dao.ReviewDAO;
 
 @Service("reviewService")
@@ -16,11 +19,20 @@ public class ReviewServiceImpl implements ReviewService {
 	
 	@Resource(name="reviewDAO")
 	private ReviewDAO reviewDAO;
-
+	
+	@Resource(name="fileUtils")
+	private FileUtils fileUtils;
 
 	@Override
-	public void insertReview(Map<String, Object> map) throws Exception {
-		reviewDAO.insertReview(map);
+	public void insertReview(Map<String, Object> map, MultipartFile[] uploadFile) throws Exception {
+		
+		reviewDAO.insertReview(map); //리뷰등록
+		
+		//-----------여기서부터 따로 클래스로 빼서 메서드로 만들예정-------------
+		List<Map<String,Object>> list = fileUtils.parseInsertFileInfo(map, uploadFile);
+		for(int i=0, size=list.size(); i<size; i++) {
+			reviewDAO.insertPhoto(list.get(i));
+		}
 	}
 
 }
