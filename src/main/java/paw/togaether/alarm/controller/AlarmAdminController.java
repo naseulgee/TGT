13fe524ed60c6abefac7a26c7040a9f7dcf3a780
naseulgee <thwndglgkwk@naver.com>
2023-02-17 19:55:26 +0900

    23.02.17 나슬기 알람 관련 패키지 및 클래스 생성

---
+++ src/main/java/paw/togaether/alarm/controller/AlarmAdminController.java
@@ -0,0 +1,19 @@
+package paw.togaether.alarm.controller;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import paw.togaether.alarm.service.AlarmService;
+
+@Controller
+@RequestMapping("/admin/alarm")
+public class AlarmAdminController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	//알람 관련 서비스
+	@Resource(name="alarmService")
+	private AlarmService alarmService;
+}
